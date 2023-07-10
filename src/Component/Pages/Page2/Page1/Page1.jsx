8d374608101d49ee8d7e779a6a29f8068d783583
unknown <emmansairun@gmail.com>
2023-07-10 16:03:05 +0100

    edited the UI of the Landing page

--- src/Component/Pages/Page2/Page1/Page1.jsx
+++ src/Component/Pages/Page2/Page1/Page1.jsx
@@ -2,34 +2,54 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import Button from '../../../Button/Button';
 import './Page1.css';
+import image from '../../../../images/workshop.jpg';
 
 function Page1() {
   return (
     <div className="page-instruction">
-      <h5>Instructions</h5>
-      <p>
-        Welcome to our question and answering seesion for this semester. Below
+      <h1>I-QUIZ</h1>
+      <h2>
+        Welcome to our question and answering session for this semester. Below
         are the instructions to guide you answer the questions.
-      </p>
-      <ul>
-        <li>Answer all the questions </li>
-        <li>
-          There are two checkboxes, that is, TRUE and FALSE. TRUE is for the
-          right answer and FALSE for the wrong answer.
-        </li>
-        <li>
-          There is a NEXT BUTTON below every question to permit you access the
-          next pageto answer the next question.
-        </li>
-        <li>
-          Once you access a page and go to the next page you wont be able to go
-          back to the previous page.
-        </li>
-      </ul>
-      <h2>GOOD LUCK!</h2>
-      <Link to="/question/0">
-        <Button />
-      </Link>
+      </h2>
+      <div className="quizMainContainer">
+        <div className="instructions">
+          <div className="zerop">
+            <p>
+              1. There are two checkboxes, that is, TRUE and FALSE. TRUE is for
+              the right answer and FALSE for the wrong answer.
+            </p>
+          </div>
+
+          <div className="firstp">
+            <p>
+              2. There is a NEXT BUTTON below every question to permit you
+              access the next pageto answer the next question.
+            </p>
+          </div>
+          <div className="secondp">
+            <p>
+              3. Remember to answer all the questions any unaswered question
+              will be considered wrong
+            </p>
+          </div>
+          <div className="thirdp">
+            <p>
+              4. Once you access a page and go to the next page you wont be able
+              to go back to the previous page.
+            </p>
+          </div>
+        </div>
+        <div>
+          <img src={image} alt="background" />
+        </div>
+      </div>
+      <div className="btmrole">
+        <h2>GOOD LUCK!</h2>
+        <Link to="/question/0">
+          <Button />
+        </Link>
+      </div>
     </div>
   );
 }
